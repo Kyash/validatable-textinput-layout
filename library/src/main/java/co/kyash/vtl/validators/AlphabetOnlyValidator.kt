189d0eb1f7ea76407b5e0eff461a9d5aa06ddae1
konifar <yahpeycoy0403@gmail.com>
2018-09-28 10:18:14 +0900

    Added alphabet only validator

---
+++ library/src/main/java/co/kyash/vtl/validators/AlphabetOnlyValidator.kt
@@ -0,0 +1,38 @@
+package co.kyash.vtl.validators
+
+import android.content.Context
+import android.text.TextUtils
+import co.kyash.vtl.VtlValidationFailureException
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
+import java.util.regex.Pattern
+
+/**
+ * Validation error when the text is not written by Alphabet
+ */
+class AlphabetOnlyValidator(
+        private val errorMessage: String
+) : VtlValidator {
+
+    companion object {
+        private val PATTERN = Pattern.compile("^[a-zA-Z]+\$")
+    }
+
+    override fun validateAsCompletable(context: Context, text: String?): Completable {
+        return Completable.fromRunnable {
+            if (!validate(text)) {
+                throw VtlValidationFailureException(errorMessage)
+            }
+        }.subscribeOn(Schedulers.computation())
+    }
+
+    override fun validate(text: String?): Boolean {
+        val trimText = text?.replace(" ", "")?.replace("　", "")?.trim()
+        return TextUtils.isEmpty(trimText) || PATTERN.matcher(trimText).matches()
+    }
+
+    override fun getErrorMessage(): String {
+        return errorMessage
+    }
+
+}
